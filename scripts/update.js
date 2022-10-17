31391ca427400652a7fa66dfcc4a7b26c00ddcc2
wilkyrlx <john_wilkinson@brown.edu>
2022-10-17 10:24:05 -0400

    events bar, dynamic

---
+++ scripts/update.js
@@ -0,0 +1,27 @@
+function updateEvents() {
+    var oFrame = document.getElementById("frmFile");
+    var strRawContents = oFrame.contentWindow.document.body.childNodes[0].innerHTML;
+
+    var arr = strRawContents.split("\n");
+    var annList = document.getElementById("events-list");
+    var docFrag = document.createDocumentFragment();
+
+    for (var i = 0; i < arr.length; i++) {
+
+        let re = /[^|]+/g
+        let parsedText = arr[i].match(re)
+        let eventTitle = parsedText[0];
+        let eventDate = parsedText[1];
+        let eventDescription = parsedText[2];
+
+        var tempNode = document.querySelector("a[data-type='template']").cloneNode(true);
+        tempNode.querySelector("h5").innerText = eventTitle;
+        tempNode.querySelector("small").innerText = eventDate;
+        tempNode.querySelector("p").innerText = eventDescription;
+        tempNode.style.display = "block";
+        docFrag.appendChild(tempNode);
+    }
+    
+    annList.appendChild(docFrag);
+    delete docFrag;
+}
