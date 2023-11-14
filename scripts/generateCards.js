a08ac97d825c75c16469f6f46cf352b8f5bafa35
John Wilkinson <93553222+wilkyrlx@users.noreply.github.com>
2023-11-14 11:50:26 -0500

    add personnel

--- scripts/generateCards.js
+++ scripts/generateCards.js
@@ -1,44 +1,305 @@
 
 // president JSON
-const prezCardData = [{
-    "timestamp": "2022/10/24 10:19:12 PM AST",
-    "name": "Karan Kashyap",
-    "role": "Co-President",
-    "classYear": 2025,
-    "about": "Karan is a sophomore from New Delhi, India, studying Computer Science and Behavioral Decision Sciences. Outside of the classroom, he loves solving puzzles, playing tennis, and building software.",
-    "email": "karan_kashyap@brown.edu",
-    "linkedin": "https://www.linkedin.com/in/karan-kashyap04/",
-    "fullImageLink": "",
-    "imageName": "karan-k.jpg"
-},
-{
-    "timestamp": "2022/09/26 2:19:35 PM AST",
-    "name": "Benjamin Moshes",
-    "role": "Co-President",
-    "classYear": 2025,
-    "about": "Benjamin is a junior from Newton, MA concentrating in Applied Math and Economics. He is also a member of the Finance Team on the Brown Daily Herald. In his free time, Benjamin loves watching NFL football and making scallion pancakes.",
-    "email": "benjamin_moshes@brown.edu",
-    "linkedin": "https://www.linkedin.com/in/benjamin-moshes-39a1a820b/",
-    "fullImageLink": "https://drive.google.com/u/0/open?usp=forms_web&id=1HNRCeFLGb62bMwv5zyMWMykBCP90CPXP",
-    "imageName": "benjamin-m.jpg"
-},
-{
-    "timestamp": "2022/10/09 2:05:00 PM AST",
-    "name": "Danna Ofek",
-    "role": "Co-President",
-    "classYear": 2024,
-    "about": "Danna is a senior from Lexington, MA concentrating in Business Economics and Public Health. She is also a member of Brown Women’s Cross Country and Track teams. In her free time, Danna enjoys the outdoors and doing yoga.",
-    "email": "danna_ofek@brown.edu",
-    "linkedin": "https://www.linkedin.com/in/danna-ofek-12b4aa23b/",
-    "fullImageLink": "https://drive.google.com/u/0/open?usp=forms_web&id=1Duh0rdOWxepx9Woi37NLFRSTk5DZry-S",
-    "imageName": "danna-o.jpg"
-}]
+const prezCardData = [
+    {
+      "timestamp": "2023/10/24 4:41:05 PM EST",
+      "name": "Karan Kashyap",
+      "role": "President",
+      "classYear": 2025,
+      "about": "Karan is a junior from New Delhi, India, studying Computer Science. He is also the President of Full Stack @ Brown. In his free time, he enjoys playing tennis and learning about AI. Next summer, Karan is going to be an intern at Jane Street in their NYC office.",
+      "email": "karan_kashyap@brown.edu",
+      "linkedin": "https://www.linkedin.com/in/karan-kashyap04",
+      "imageName": "karan-k.jpg"
+    },
+    {
+      "timestamp": "2023/10/31 9:57:25 PM EST",
+      "name": "Benjamin Moshes",
+      "role": "President",
+      "classYear": 2025,
+      "about": "Benjamin is a junior from Newton, Massachusetts studying Applied Math and Economics. Outside of CCG, he serves as the General Manager of the Brown Daily Herald, works as an education economics research assistant, and volunteers teaching Ukrainian refugees English. Next summer, he is looking forward to joining the Boston Consulting Group as a summer intern.",
+      "email": "benjaminm3030@gmail.com",
+      "linkedin": "https://www.linkedin.com/in/benjamin-moshes-39a1a820b/",
+      "imageName": "benjamin-m.jpg"
+    },
+    {
+      "timestamp": "2023/11/01 8:30:22 AM EST",
+      "name": "Danna Ofek",
+      "role": "President",
+      "classYear": 2024,
+      "about": "Danna is a senior from Lexington, MA concentrating in Business Economics and Public Health. She is also a member of Brown’s Cross Country and Track & Field teams. Danna will be joining EY-Parthenon full-time after graduation.",
+      "email": "danna_ofek@brown.edu",
+      "linkedin": "https://www.linkedin.com/in/danna-ofek-12b4aa23b/",
+      "imageName": "danna-o.jpg"
+    }
+  ]
 
 // leadership JSON (do not include presidents in this list)
-const leadershipCardData = []
+const leadershipCardData = [
+    {
+        "timestamp": "2023/11/01 12:22:54 PM EST",
+        "name": "John Wilkinson",
+        "role": "Internal Development Lead",
+        "classYear": 2025,
+        "about": "John is a junior from Dedham, MA, studying Computer Science and Archaeology. Outside of the classroom, he enjoys playing poker and windsurfing.",
+        "email": "john_wilkinson@brown.edu",
+        "linkedin": "https://www.linkedin.com/in/john-wilkinson2025/",
+        "imageName": "john-w.jpg"
+    },
+    {
+      "timestamp": "2023/10/23 4:50:03 PM EST",
+      "name": "Ashley Ganesh",
+      "role": "Internal Development Lead",
+      "classYear": 2025,
+      "about": "Ashley is a junior from Baldwin, NY concentrating in Business Economics and International & Public Affairs. She is also a member of the Brown Undergraduate Law Review and Brown Women in Business. In her free time, she enjoys going on hikes and learning new recipes.",
+      "email": "ashley_ganesh@brown.edu",
+      "linkedin": "https://www.linkedin.com/in/ashleyganesh/",
+      "imageName": "ashley-g.jpg"
+    },
+    {
+        "timestamp": "2023/10/22 9:21:47 PM EST",
+        "name": "PengCheng Zhu",
+        "role": "Project Leader",
+        "classYear": 2026,
+        "about": "PengCheng is a sophomore from Irvine, CA, concentrating in Business Economics and Chemistry. Outside of the classroom, he enjoys composing and going to concerts.",
+        "email": "pengcheng_zhu@brown.edu",
+        "linkedin": "https://www.linkedin.com/in/pengcheng-zhu-4178bb242/",
+        "imageName": "pengcheng-z.jpg"
+    },
+    {
+      "timestamp": "2023/10/25 8:11:19 PM EST",
+      "name": "Vincent Moroz",
+      "role": "Project Leader",
+      "classYear": 2025,
+      "about": "Vincent is a junior from Austin, TX, studying Business Economics and Modern Culture & Media. In his free time he enjoys writing music, singing with his band SKIRMISH, and playing tennis.",
+      "email": "vincent_moroz@brown.edu",
+      "linkedin": "https://www.linkedin.com/in/vincent-moroz/",
+      "imageName": "vincent-m.jpg"
+    },
+    {
+      "timestamp": "2023/10/31 9:27:06 PM EST",
+      "name": "Jasper Wang",
+      "role": "Project Manager",
+      "classYear": 2025,
+      "about": "Jasper is a junior from Palo Alto, CA, studying Applied Math and Economics. He is also a member of Brown's Club Golf team, GAMMA, and the Climbing Club. In his free time, he enjoys playing golf and building mechanical keyboards.",
+      "email": "jasper_wang@brown.edu",
+      "linkedin": "https://www.linkedin.com/in/jasper-wang-00b189166/",
+      "imageName": "jasper-w.jpg"
+    },
+    {
+      "timestamp": "2023/11/01 12:22:54 PM EST",
+      "name": "Haadi Iqbal",
+      "role": "Project Manager",
+      "classYear": 2024,
+      "about": "Haadi is a senior from Karachi, Pakistan and Toronto, Canada studying Computer Science and Economics. In his free time, he enjoys cooking and watching cricket.",
+      "email": "haadi_iqbal@brown.edu",
+      "linkedin": "",
+      "imageName": "haadi-i.jpg"
+    }
+  ]
 
 // consultant JSON
-const consultantCardData = []
+const consultantCardData = [
+    {
+      "timestamp": "2023/10/22 8:53:21 PM EST",
+      "name": "Albert Lou",
+      "role": "Consultant",
+      "classYear": 2026,
+      "about": "Albert is a sophomore from Toronto, Canada concentrating in Economics and International and Public Affairs. He is also a member of the Brown University Band for which he plays the Alto Saxophone. In his free time, Albert enjoys running, cooking, and creative writing.",
+      "email": "albert_lou@brown.edu",
+      "linkedin": "https://www.linkedin.com/in/albert-lou/",
+      "imageName": "albert-l.jpg"
+    },
+    {
+      "timestamp": "2023/10/22 8:53:28 PM EST",
+      "name": "Krish Shah",
+      "role": "Consultant",
+      "classYear": 2026,
+      "about": "Krish is a sophomore from Walpole, MA concentrating in Neuroscience and Business Economics. He is also a student research assistant at the Brown University School of Public Health and at Massachusetts General Hospital. In his free time, Krish likes to hike and watch football.",
+      "email": "krish_shah2@brown.edu",
+      "linkedin": "https://www.linkedin.com/in/krish-shah-932a19240/",
+      "imageName": "krish-s.jpg"
+    },
+    // {
+    //   "timestamp": "2023/10/22 9:11:41 PM EST",
+    //   "name": "Adrien Durantel",
+    //   "role": "Consultant",
+    //   "classYear": 2027,
+    //   "about": "Adrien is a freshman from London, UK. He plans on concentrating in APMA-CS. In his free time he enjoys swimming and just enjoying the small things in life.",
+    //   "email": "adrien_durantel@brown.edu",
+    //   "linkedin": "",
+    //   "imageName": "adrien-d.jpg"
+    // },
+    {
+      "timestamp": "2023/10/22 9:23:20 PM EST",
+      "name": "Noah Hallward-Rough",
+      "role": "Consultant",
+      "classYear": 2026,
+      "about": "Noah is a Sophomore from Saco, ME, studying Environmental Studies and Business Economics. He is also a member of Brown Tutoring Corps and Brown EP. Outside of the classroom, he enjoys intramural sports and listening to music.",
+      "email": "noah_hallward-rough@brown.edu",
+      "linkedin": "https://www.linkedin.com/in/noah-hallward-rough-98b8a1267/",
+      "imageName": "noah-h.jpg"
+    },
+    {
+      "timestamp": "2023/10/22 9:41:07 PM EST",
+      "name": "Tanay Subramanian",
+      "role": "Consultant",
+      "classYear": 2026,
+      "about": "Tanay is a sophomore from San Ramon, CA concentrating in Applied Math-Computer Science and Economics. He is also a part of the Brown Investment Group and Socially Responsible Investment Fund. Outside of the classroom, Tanay enjoys backpacking and playing the alto saxophone.",
+      "email": "tanay_subramanian@brown.edu",
+      "linkedin": "https://www.linkedin.com/in/tanay-subramanian/",
+      "imageName": "tanay-s.jpg"
+    },
+    {
+      "timestamp": "2023/10/22 9:58:27 PM EST",
+      "name": "Andrew Zhong",
+      "role": "Consultant",
+      "classYear": 2026,
+      "about": "Andrew is a sophomore from Clarksville, MD concentrating in Business Economics and Statistics. He is also a member of the Intercollegiate Finance Journal and Entrepreneurship Program. In his free time, Andrew enjoys learning card tricks.",
+      "email": "andrew_zhong@brown.edu",
+      "linkedin": "https://www.linkedin.com/in/andrew-zhong-1ab01317a",
+      "imageName": "andrew-z.jpg"
+    },
+    {
+      "timestamp": "2023/10/23 8:19:08 AM EST",
+      "name": "Kelly Jeong",
+      "role": "Internal Development",
+      "classYear": 2026,
+      "about": "Kelly is a sophomore from Duluth, GA studying Applied Mathematics, Economics, and Computer Science. Outside of school, she enjoys drawing and playing the violin.",
+      "email": "kelly_jeong@brown.edu",
+      "linkedin": "https://www.linkedin.com/in/kelly-jeong/",
+      "imageName": "kelly-j.jpg"
+    },
+    {
+      "timestamp": "2023/10/23 9:43:53 AM EST",
+      "name": "Cole Griscom",
+      "role": "Internal Development",
+      "classYear": 2025,
+      "about": "Cole is a junior from outside of Philadelphia, PA concentrating in Business Economics and Public Health. On campus, he is also involved with Connect for Health and the Healthcare Investment Group. In his free time, Cole sings for the University Chorus and enjoys crocheting.",
+      "email": "cole_griscom@brown.edu",
+      "linkedin": "https://www.linkedin.com/in/cole-griscom-a05b89229/",
+      "imageName": "cole-g.jpg"
+    },
+    {
+      "timestamp": "2023/10/23 10:27:30 AM EST",
+      "name": "Noemi Elliott",
+      "role": "Internal Development",
+      "classYear": 2026,
+      "about": "Noemi is a sophomore from Singapore studying Applied Math - Economics and East Asian Studies. Outside of the classroom, Noemi enjoys reading 19th century British literature and dissecting classic Hollywood cinema.",
+      "email": "Noemi_elliott@brown.edu",
+      "linkedin": "",
+      "imageName": "noemi-e.jpg"
+    },
+    {
+      "timestamp": "2023/10/23 1:13:16 PM EST",
+      "name": "Elisa Carroll",
+      "role": "Consultant",
+      "classYear": 2026,
+      "about": "Elisa is a sophomore from Dallas, TX concentrating in Applied Math and English. She is also a member of Brown Women's Club Lacrosse. In her free time, she enjoys running, listening to music, and reviewing movies.",
+      "email": "elizabeth_r_carroll@gmail.com",
+      "linkedin": "https://www.linkedin.com/in/elisacarroll",
+      "imageName": "elisa-c.jpg"
+    },
+    {
+      "timestamp": "2023/10/23 10:37:05 PM EST",
+      "name": "Ciaran Henry",
+      "role": "Internal Development",
+      "classYear": 2026,
+      "about": "Ciaran is a sophomore from Haworth, NJ, concentrating in Public Policy Economics and Data Fluency. He is also involved with the Brown Art Review, Fashion@Brown, Intercollegiate Finance Journal, and HOPE. In his free time, he enjoys reading nonfiction and up-cycling clothes.",
+      "email": "ciaran_henry@brown.edu",
+      "linkedin": "",
+      "imageName": "ciaran-h.jpg"
+    },
+    {
+      "timestamp": "2023/10/24 11:50:20 AM EST",
+      "name": "Jo Jojo",
+      "role": "Consultant",
+      "classYear": 2027,
+      "about": "Jo is a freshman from Los Angeles, CA concentrating in Philosophy and Computer Science. Jo was born and raised a Lakers fan and he has an unrivaled passion for cinema.",
+      "email": "jo_jojo@brown.edu",
+      "linkedin": "https://www.linkedin.com/in/jo-jojo-18455325b/",
+      "imageName": "jo-j.jpg"
+    },
+    {
+      "timestamp": "2023/10/24 2:41:23 PM EST",
+      "name": "Katrina Sun",
+      "role": "Consultant",
+      "classYear": 2026,
+      "about": "Katrina is a sophomore from Vancouver, CA intending on concentrating in Business Economics and East Asian Studies. She is also a member of Brown's Socially Responsible Investment Fund and Women in Business. In her free time, Katrina enjoys exploring cafes in Providence and hiking.",
+      "email": "katrina_sun@brown.edu",
+      "linkedin": "",
+      "imageName": "katrina-s.jpg"
+    },
+    {
+      "timestamp": "2023/10/24 4:02:58 PM EST",
+      "name": "Audrey Chou",
+      "role": "Consultant",
+      "classYear": 2026,
+      "about": "Audrey is a sophomore from Queens, NY concentrating in Urban Studies and Computer Science. In her free time, she enjoys chai lattes and thrifting with friends!",
+      "email": "audrey_chou@brown.edu",
+      "linkedin": "",
+      "imageName": "audrey-c.jpg"
+    },
+    {
+      "timestamp": "2023/10/26 11:34:19 PM EST",
+      "name": "Adyant Ranjan",
+      "role": "Consultant",
+      "classYear": 2027,
+      "about": "Adyant is a first-year from Orlando, FL intending to concentrate in Applied Math-Computer Science and Economics. In his free time, he enjoys playing tennis and watching classic films.",
+      "email": "adyant_ranjan@brown.edu",
+      "linkedin": "www.linkedin.com/in/adyant-ranjan",
+      "imageName": "adyant-r.jpg"
+    },
+    {
+      "timestamp": "2023/10/28 1:49:41 AM EST",
+      "name": "Harshil Garg",
+      "role": "Consultant",
+      "classYear": 2026,
+      "about": "Harshil is a sophomore from Los Gatos, CA, studying IAPA and Archaeology. He is also a member of Brown's MUN club, BPR, and the Wind Symphony. In his free time, Harshil enjoys traveling to historic sites and museums.",
+      "email": "harshil_garg@brown.edu",
+      "linkedin": "",
+      "imageName": "harshil-g.jpg"
+    },
+    {
+      "timestamp": "2023/11/01 8:39:31 AM EST",
+      "name": "Jasmine Wang",
+      "role": "Internal Development",
+      "classYear": 2024,
+      "about": "Jasmine is a senior from Hong Kong studying Business Economics and English. She is also the Co-President of Brown Cooking Club and enjoys cooking and hosting themed dinner parties for her friends.",
+      "email": "jasmine_t_wang@brown.edu",
+      "linkedin": "",
+      "imageName": "jasmine-w.jpg"
+    },
+    {
+      "timestamp": "2023/11/01 9:08:01 AM EST",
+      "name": "Sujith Pakala",
+      "role": "Consultant",
+      "classYear": 2026,
+      "about": "Sujith is a sophomore from San Jose, CA studying Computer Science-Economics and Biology. He is also a member of Van Wickle Ventures and Brown Badmaash and enjoys playing pikeball on the main green in his free time.",
+      "email": "sujith_pakala@brown.edu",
+      "linkedin": "https://www.linkedin.com/in/sujithpakala/",
+      "imageName": "sujith-p.jpg"
+    },
+    // {
+    //   "timestamp": "2023/11/01 10:05:52 AM EST",
+    //   "name": "Ethan Chuang",
+    //   "role": "Consultant",
+    //   "classYear": 2026,
+    //   "about": "Ethan is a sophomore from San Diego, MA concentrating in Applied Math and Economics. Outside of the classroom, he enjoys playing the clarinet and piano.",
+    //   "email": "ethan_chuang@brown.edu",
+    //   "linkedin": "",
+    //   "imageName": "ethan-c.jpg"
+    // },
+    {
+      "timestamp": "2023/11/02 11:51:33 AM EST",
+      "name": "Zubin Oommen",
+      "role": "Consultant",
+      "classYear": 2027,
+      "about": "Zubin is a freshman from Seattle, MA, studying Computer Science and Economics. Outside of school, Zubin loves to play tennis, watch football, and lose his money in poker.",
+      "email": "zubin_oommen@brown.edu",
+      "linkedin": "",
+      "imageName": "zubin-o.jpg"
+    }
+  ]
 
 // builds HTML cards for each person given relevant strings
 function createCard(name, role, classYear, about, email, linkedin, imageName) {
